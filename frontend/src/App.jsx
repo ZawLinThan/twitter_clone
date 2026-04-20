7b711a26f64149e2191557ae2c832e0ff78d1ae4
Zawlinthan <zawlinthan2005@gmail.com>
2026-04-20 09:34:34 -0700

    2 - UI design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
 import {Routes, Route} from 'react-router-dom'
-import HomePage from "../src/pages/auth/home/HomePage.jsx"
+import HomePage from "./pages/home/HomePage.jsx"
 import LoginPage from "../src/pages/auth/login/LoginPage.jsx"
 import SignupPage from './pages/auth/signup/SignupPage.jsx'
+import NotificationPage from './pages/notification/NotificationPage.jsx'
+import ProfilePage from './pages/profile/ProfilePage.jsx'
+
+
+import Sidebar from './components/common/Sidebar.jsx'
+import RightPanel from './components/common/RightPanel.jsx'
+
 
 function App() {
 
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar/> 
       <Routes> 
         <Route path="/" element={<HomePage/>}/> 
         <Route path="/login" element={<LoginPage/>}/> 
         <Route path="/signup" element={<SignupPage/>}/> 
+        <Route path="/notifications" element={<NotificationPage/>}/>
+        <Route path="/profile/:username" element={<ProfilePage/>}/>
       </Routes>
+      <RightPanel/>
     </div>
   )
 }
